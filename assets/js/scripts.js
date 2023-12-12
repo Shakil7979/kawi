efad1dfc7e6b2e2472a11d99255f723b881c9b2c
Sadbin Shakil <sadbin7979@gmail.com>
2023-12-12 19:28:24 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,6 +14,19 @@ $(document).ready(function(){
 		return false
 	});
 
+	$('.menu_icon').click(function(){
+		$('.menu').slideToggle(200);
+
+		return false
+	}); 
+
+
+	// Mobile Menu Icon
+	$(document).ready(function(){
+		$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+			$(this).toggleClass('open');
+		});
+	});
 
 
 
